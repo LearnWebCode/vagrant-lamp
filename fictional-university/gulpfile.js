52067041b9403d067d984b7e3736c1e941926ddd
Brad <brad@learnwebcode.com>
2017-11-18 13:13:32 -0800

    Added error handling for CSS gulp task.

--- fictional-university/gulpfile.js
+++ fictional-university/gulpfile.js
@@ -14,6 +14,7 @@ colorFunctions = require('postcss-color-function');
 gulp.task('styles', function() {
   return gulp.src(settings.themeLocation + 'css/style.css')
     .pipe(postcss([cssImport, mixins, cssvars, nested, rgba, colorFunctions, autoprefixer]))
+    .on('error', (error) => console.log(error.toString()))
     .pipe(gulp.dest(settings.themeLocation));
 });
 
