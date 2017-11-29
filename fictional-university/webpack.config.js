da78f1997662e0e5c491118942f4109e1e074c28
Brad <brad@learnwebcode.com>
2017-11-29 12:45:53 -0800

    Updated babel preset from es2015 to env.

--- fictional-university/webpack.config.js
+++ fictional-university/webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
       {
         loader: 'babel-loader',
         query: {
-          presets: ['es2015']
+          presets: ['env']
         },
         test: /\.js$/,
         exclude: /node_modules/
