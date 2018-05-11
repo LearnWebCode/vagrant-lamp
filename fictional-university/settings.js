f45d4c677259e4746a33a92e54f2eefad0aca562
Brad Schiff <brad@learnwebcode.com>
2018-05-11 12:25:31 -0700

    Added notes to settings.js

--- fictional-university/settings.js
+++ fictional-university/settings.js
@@ -9,3 +9,6 @@ exports.urlToPreview = 'http://fictional-university.test';
 
 // Simply remove the two slashes at the front of those lines
 // to uncomment them and then delete lines #1 and #2.
+
+// Be SURE to update urlToPreview to YOUR domain and not mine.
+// Be SURE to update themeLocation to YOUR theme folder name
