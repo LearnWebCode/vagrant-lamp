e843dcc73d4ec4151493e659de5a4857c212c507
Brad Schiff <brad@learnwebcode.com>
2018-05-11 12:18:37 -0700

    Updated package versions and included example values in settings.js

--- fictional-university/settings.js
+++ fictional-university/settings.js
@@ -1,2 +1,11 @@
 exports.themeLocation = './app/wp-content/themes/fictional-university-theme/';
 exports.urlToPreview = 'http://fictional-university.test';
+
+// If you're using Local by Flywheel you will
+// want your settings to be similar to the examples below:
+
+// exports.themeLocation = './public/wp-content/themes/fictional-university-theme/';
+// exports.urlToPreview = 'http://fictional-university.local/';
+
+// Simply remove the two slashes at the front of those lines
+// to uncomment them and then delete lines #1 and #2.
