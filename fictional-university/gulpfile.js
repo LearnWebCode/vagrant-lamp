7a1936cad5bac41f061904f545902c43933b2c33
ppab <josearmentab@gmail.com>
2020-04-13 23:38:50 -0500

    fix for browser sync not reloading after first save

--- fictional-university/gulpfile.js
+++ fictional-university/gulpfile.js
@@ -1,25 +1,36 @@
-var gulp = require('gulp'),
-settings = require('./settings'),
-webpack = require('webpack'),
-browserSync = require('browser-sync').create(),
-postcss = require('gulp-postcss'),
-rgba = require('postcss-hexrgba'),
-autoprefixer = require('autoprefixer'),
-cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested'),
-cssImport = require('postcss-import'),
-mixins = require('postcss-mixins'),
-colorFunctions = require('postcss-color-function');
+var gulp = require("gulp"),
+  settings = require("./settings"),
+  webpack = require("webpack"),
+  browserSync = require("browser-sync").create(),
+  postcss = require("gulp-postcss"),
+  rgba = require("postcss-hexrgba"),
+  autoprefixer = require("autoprefixer"),
+  cssvars = require("postcss-simple-vars"),
+  nested = require("postcss-nested"),
+  cssImport = require("postcss-import"),
+  mixins = require("postcss-mixins"),
+  colorFunctions = require("postcss-color-function");
 
-gulp.task('styles', function() {
-  return gulp.src(settings.themeLocation + 'css/style.css')
-    .pipe(postcss([cssImport, mixins, cssvars, nested, rgba, colorFunctions, autoprefixer]))
-    .on('error', (error) => console.log(error.toString()))
+gulp.task("styles", function () {
+  return gulp
+    .src(settings.themeLocation + "css/style.css")
+    .pipe(
+      postcss([
+        cssImport,
+        mixins,
+        cssvars,
+        nested,
+        rgba,
+        colorFunctions,
+        autoprefixer,
+      ])
+    )
+    .on("error", (error) => console.log(error.toString()))
     .pipe(gulp.dest(settings.themeLocation));
 });
 
-gulp.task('scripts', function(callback) {
-  webpack(require('./webpack.config.js'), function(err, stats) {
+gulp.task("scripts", function (callback) {
+  webpack(require("./webpack.config.js"), function (err, stats) {
     if (err) {
       console.log(err.toString());
     }
@@ -29,26 +40,40 @@ gulp.task('scripts', function(callback) {
   });
 });
 
-gulp.task('watch', function() {
+gulp.task("watch", function () {
   browserSync.init({
     notify: false,
     proxy: settings.urlToPreview,
-    ghostMode: false
+    ghostMode: false,
   });
 
-  gulp.watch('./**/*.php', function() {
-    browserSync.reload();
-  });
-  gulp.watch(settings.themeLocation + 'css/**/*.css', gulp.parallel('waitForStyles'));
-  gulp.watch([settings.themeLocation + 'js/modules/*.js', settings.themeLocation + 'js/scripts.js'], gulp.parallel('waitForScripts'));
+  gulp.watch("./**/*.php").on("change", browserSync.reload);
+  gulp.watch(
+    settings.themeLocation + "css/**/*.css",
+    gulp.parallel("waitForStyles")
+  );
+  gulp.watch(
+    [
+      settings.themeLocation + "js/modules/*.js",
+      settings.themeLocation + "js/scripts.js",
+    ],
+    gulp.parallel("waitForScripts")
+  );
 });
 
-gulp.task('waitForStyles', gulp.series('styles', function() {
-  return gulp.src(settings.themeLocation + 'style.css')
-    .pipe(browserSync.stream());
-}))
+gulp.task(
+  "waitForStyles",
+  gulp.series("styles", function () {
+    return gulp
+      .src(settings.themeLocation + "style.css")
+      .pipe(browserSync.stream());
+  })
+);
 
-gulp.task('waitForScripts', gulp.series('scripts', function(cb) {
-  browserSync.reload();
-  cb()
-}))
+gulp.task(
+  "waitForScripts",
+  gulp.series("scripts", function (cb) {
+    browserSync.reload();
+    cb();
+  })
+);
